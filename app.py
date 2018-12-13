83379efb3de1f33ced874b48370910a6d4f16d7c
owenofengland <owen.england98@gmail.com>
2018-12-13 18:35:44 -0500

    adding s3 for qr and music

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ import ssl
 import qrcode
 import random
 import string
-from io import StringIO
+#from io import StringIO was not working so imported StringIO
+import StringIO
 import datetime
 import geocoder
 import json
 import requests
 from PIL import Image
-
+import boto3
+from botocore.client import Config
 
 
 ip = "0.0.0.0"
@@ -30,6 +32,7 @@ app.config['MONGO_URI'] = 'mongodb://mongodb:27017/'
 mongo = PyMongo(app)
 
 
+
 Client = MongoClient("localhost:27017")
 db = Client["Library"]
 userlist = db["Users"]
@@ -41,138 +44,154 @@ app.secret_key = "super secret key"
 
 @app.route('/')
 def home_page():
-    if 'username' not in session:
-        return render_template("home.html")
-    else:
-        return render_template("home.html", username = session["username"])
+	if 'username' not in session:
+		return render_template("home.html")
+	else:
+		return render_template("home.html", username = session["username"])
 
-@app.route('/<code>')
+@app.route('/<code>', methods=['GET', 'POST'])
 def imagec(code):
-    if(code == "favicon.ico"):
-        return redirect('/')
-    else:
-        song = {}
-        for i in songlist.find({'code':code}):
-            song = i
-        #host = "104.196.173.11"
-        #host = "104.196.26.6"
-        #host = "localhost"
-        # CHANGE THIS HOST
-        host = "10.16.35.99"
-        imagelist = []
-        clist = string.ascii_lowercase + string.ascii_uppercase + string.digits 
-        for i in range(10):
-            n = 30
-            gencode = ''
-            for i in range(n):
-                gencode = gencode + random.choice(clist)
-            gencode = gencode + code
-            durl = "http://" + host + ":" + str(port) + "/downloadfile/" + gencode
-            img = qrcode.make(durl) # img is a png image
-            imgname = 'static/image/' + str(gencode) + '.png'
-            img.save(imgname)
-            imagelist.append(imgname)
-            downloadlist.insert({"scode":code, "dcode":gencode, "time":"None", "loc":"None", "scanned":False}) # avoid duplicates
-        return render_template("home.html", username = session["username"], imagelist = imagelist)
-
-
+	if(code == "favicon.ico"):
+		return redirect('/')
+	else:
+		s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
+		song = {}
+		for i in songlist.find({'code':code}):
+			song = i
+		#host = "104.196.173.11"
+		#host = "104.196.26.6"
+		#host = "localhost"
+		# CHANGE THIS HOST
+		#host = "10.16.35.99"
+		host = 'localhost'
+		imagelist = []
+		clist = string.ascii_lowercase + string.ascii_uppercase + string.digits 
+		for i in range(10):
+			thisKey = code + ':' + song['filename'] 
+			durl = s3.generate_presigned_url(ClientMethod='get_object', Params={
+				'Bucket': 'lswafinal-songdb',
+				'Key': thisKey
+				},
+				ExpiresIn = 43200
+			)
+			response = requests.get(durl)
+			print(response.url)
+			n = 30
+			gencode = ''
+			for i in range(n):
+				gencode = gencode + random.choice(clist)
+			gencode = gencode + code
+			img = qrcode.make(response.url)
+			imgname = 'static/image/' + str(gencode) + '.png'
+			buffer = StringIO.StringIO()
+			img.save(buffer, 'PNG')
+			image = buffer.getvalue()
+			img.save(imgname)
+			toBucket = boto3.resource('s3')
+			imgKey = str(gencode) + '.png'
+			toBucket.Bucket('lswafinal-qr').put_object(Key=imgKey, Body=image)
+			imagelist.append(imgname)
+			downloadlist.insert({'scode':code, 'dcode':gencode, 'time':'None', 'loc':'None', 'scanned':False})
+		return render_template('home.html', username=session['username'], imagelist=imagelist)
 
 @app.route('/download', methods = ['GET', 'POST'])
 def download():
-    if request.method == "POST":
-        code = request.form['code']
-        song = {}
-        found = False
-        for i in songlist.find({'code':code}):
-            found = True
-            song = i
-        if found == False:
-            if 'username' not in session:
-                return render_template("download.html", message = "This code is not valid")
-            else:
-                return render_template("download.html", username = session["username"], message = "This code is not valid")
-        else:
-            if 'username' not in session:
-                return render_template("download.html", song = song)
-            else:
-                return render_template("download.html", username = session["username"], song = song)        
-    else:
-        if 'username' not in session:
-            return render_template("download.html")
-        else:
-            return render_template("download.html", username = session["username"])
+	if request.method == "POST":
+		code = request.form['code']
+		song = {}
+		found = False
+		for i in songlist.find({'code':code}):
+			found = True
+			song = i
+		if found == False:
+			if 'username' not in session:
+				return render_template("download.html", message = "This code is not valid")
+			else:
+				return render_template("download.html", username = session["username"], message = "This code is not valid")
+		else:
+			if 'username' not in session:
+				return render_template("download.html", song = song)
+			else:
+				return render_template("download.html", username = session["username"], song = song)        
+	else:
+		if 'username' not in session:
+			return render_template("download.html")
+		else:
+			return render_template("download.html", username = session["username"])
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-        if request.method == "POST":
-            username = request.form['username']
-            hashed_password = hashlib.sha1((request.form['password']).encode('utf-8'))
-            exists = False 
-            for i in userlist.find({'username':username}):
-                exists = True
-            if exists == True:
-                match = False 
-                for i in userlist.find({}):
-                    pprint.pprint(i)
-                for i in userlist.find({'username':username, 'password':hashed_password.hexdigest()}):
-                    match = True
-                if match == True:
-                    session['username'] = username
-                    return redirect("/")
-                else:
-                    return render_template("login.html", message = "Username and password don't match!")
-            else:
-                return render_template("login.html", message = "Username does not exist!")
-        else:
-            return render_template("login.html")
+		if request.method == "POST":
+			username = request.form['username']
+			hashed_password = hashlib.sha1((request.form['password']).encode('utf-8'))
+			exists = False 
+			for i in userlist.find({'username':username}):
+				exists = True
+			if exists == True:
+				match = False 
+				for i in userlist.find({}):
+					pprint.pprint(i)
+				for i in userlist.find({'username':username, 'password':hashed_password.hexdigest()}):
+					match = True
+				if match == True:
+					session['username'] = username
+					return redirect("/")
+				else:
+					return render_template("login.html", message = "Username and password don't match!")
+			else:
+				return render_template("login.html", message = "Username does not exist!")
+		else:
+			return render_template("login.html")
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
-        if request.method == "POST":
-            username = request.form['username']
-            if request.form['password'] != request.form['password2']:
-                return render_template("register.html", message = "Passwords must match!")
-            hashed_password = hashlib.sha1((request.form['password']).encode('utf-8'))
-            newuser = True 
-            for i in userlist.find({'username':username}):
-                newuser = False
-            if newuser == True:
-                session['username'] = username
-                userlist.insert({'username': username, 'password': hashed_password.hexdigest()})
-                return redirect("/")
-            else:
-                return render_template("register.html", message = "Username already exists!")
-        else:
-            return render_template("register.html")
+		if request.method == "POST":
+			username = request.form['username']
+			if request.form['password'] != request.form['password2']:
+				return render_template("register.html", message = "Passwords must match!")
+			hashed_password = hashlib.sha1((request.form['password']).encode('utf-8'))
+			newuser = True 
+			for i in userlist.find({'username':username}):
+				newuser = False
+			if newuser == True:
+				session['username'] = username
+				userlist.insert({'username': username, 'password': hashed_password.hexdigest()})
+				return redirect("/")
+			else:
+				return render_template("register.html", message = "Username already exists!")
+		else:
+			return render_template("register.html")
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload():
-    if request.method == "POST":
-        if 'username' not in session:
-            return render_template("upload.html", message = "You have to be logged in to upload!")
-        else:
-            title = request.form['title']
-            artist = request.form['artist']
-            music = request.files['file']
-            filename = request.files['file'].filename
-            clist = string.ascii_lowercase + string.ascii_uppercase + string.digits 
-            n = 20
-            gencode = ''
-            for i in range(n):
-                gencode = gencode + random.choice(clist)
-            hashed_user = hashlib.sha1((session['username']).encode('utf-8'))
-            now = datetime.datetime.now().strftime("%a, %d %B %Y %I:%M:%S %p")
-            gencode = gencode + hashed_user.hexdigest()
-            songlist.insert({'title': title, 'artist':artist, 'code':gencode, 'filename': filename, 'username': session['username'], 'downloads': 0, 'time':now, 'timelist':[], 'loclist':[], 'dllist':[]})
-            mscname = 'static/music/' + str(filename)
-            music.save(mscname)
-            rpath = "/" + str(gencode)
-            return redirect(rpath)
-    else:
-        if 'username' not in session:
-            return render_template("upload.html")
-        else:
-            return render_template("upload.html", username = session["username"])
+	if request.method == "POST":
+		if 'username' not in session:
+			return render_template("upload.html", message = "You have to be logged in to upload!")
+		else:
+			s3 = boto3.resource('s3')
+			title = request.form['title']
+			artist = request.form['artist']
+			music = request.files['file']
+			filename = request.files['file'].filename
+			clist = string.ascii_lowercase + string.ascii_uppercase + string.digits
+			n = 20
+			gencode = ''
+			for i in range(n):
+				gencode = gencode + random.choice(clist)
+			hashed_user = hashlib.sha1((session['username']).encode('utf-8'))
+			now = datetime.datetime.now().strftime("%a, %d %B %Y %I: %M: %S %p")
+			gencode = gencode + hashed_user.hexdigest()
+			songlist.insert({'title':title, "artist":artist, "code":gencode, "filename":filename, "username":session["username"], 'downloads':0, "time":now, 'timelist':[], 'loclist':[], 'dllist':[]})
+			s3.Bucket('lswafinal-songdb').put_object(Key=(gencode+':'+filename), Body=music)
+			mscname = 'static/music/' + str(filename)
+			music.save(mscname)
+			rpath = '/' + str(gencode)
+			return redirect(rpath)
+	else:
+		if 'username' not in session:
+			return render_template('upload.html')
+		else:
+			return render_template('upload.html', username=session['username'])
 
 
 
@@ -180,107 +199,107 @@ def upload():
 
 @app.route('/downloadfile/<code>')
 def downloadfile(code):
-    clist = string.ascii_lowercase + string.ascii_uppercase + string.digits 
-    gencode = ''
-    n = 50   
-    for i in range(n):
-        gencode = gencode + random.choice(clist)
-    if 'unique' not in session:
-        session['unique'] = gencode
-    ip = ""
-    if request.headers.getlist("X-Forwarded-For"):
-       ip = request.headers.getlist("X-Forwarded-For")[0]
-    else:
-        ip = request.remote_addr
-    cpic = session['unique'] + code # get the client ip code
-    song = {}
-    scode = ""
-    for i in downloadlist.find({'dcode':code}):
-        scode = i['scode']
-    for i in songlist.find({'code':scode}):
-        song = i
-    gurl = "http://api.ipstack.com/" + str(ip) + "?access_key=1e8045f7688859a698512a0abf267f5b"
-    response = requests.get(gurl)    
-    json_data = json.loads(response.text)
-    here = str(json_data["region_name"])
-    now = datetime.datetime.now().strftime("%a, %d %B %Y %I:%M:%S %p")
-    downloadlist.update_one({"dcode":code} , {"$set": {"time":now, "loc":here, "scanned":True}}) # avoid duplicates
-    filename = song['filename']
-    path = "static/music/" + filename
-    return send_file(path, attachment_filename=filename, as_attachment=True)
+	clist = string.ascii_lowercase + string.ascii_uppercase + string.digits 
+	gencode = ''
+	n = 50   
+	for i in range(n):
+		gencode = gencode + random.choice(clist)
+	if 'unique' not in session:
+		session['unique'] = gencode
+	ip = ""
+	if request.headers.getlist("X-Forwarded-For"):
+	   ip = request.headers.getlist("X-Forwarded-For")[0]
+	else:
+		ip = request.remote_addr
+	cpic = session['unique'] + code # get the client ip code
+	song = {}
+	scode = ""
+	for i in downloadlist.find({'dcode':code}):
+		scode = i['scode']
+	for i in songlist.find({'code':scode}):
+		song = i
+	gurl = "http://api.ipstack.com/" + str(ip) + "?access_key=1e8045f7688859a698512a0abf267f5b"
+	response = requests.get(gurl)    
+	json_data = json.loads(response.text)
+	here = str(json_data["region_name"])
+	now = datetime.datetime.now().strftime("%a, %d %B %Y %I:%M:%S %p")
+	downloadlist.update_one({"dcode":code} , {"$set": {"time":now, "loc":here, "scanned":True}}) # avoid duplicates
+	filename = song['filename']
+	path = "static/music/" + filename
+	return send_file(path, attachment_filename=filename, as_attachment=True)
 
 @app.route('/user')
 def user():
-    return render_template("user.html")
+	return render_template("user.html")
 
 @app.route('/stat')
 def stat():
-    if 'username' not in session:
-        return render_template("home.html", message = "You must be logged in to your stats!")
-    else:
-        mysonglist = []
-        for i in songlist.find({'username':session['username']}):
-            song = i
-            code = song['code']
-            timelist = song['timelist']
-            loclist = song['loclist']
-            dllist = song['dllist']
-            for j in downloadlist.find({'scode':code}):
-                if j['scanned'] == True and j['dcode'] not in dllist:
-                    #timelist.append(j['time'])
-                    #loclist.append(j['loc'])
-                    dllist.append(j['dcode'])
-            songlist.find_one_and_update({'code':code}, {"$set": {'timelist':timelist}})
-            songlist.find_one_and_update({'code':code}, {"$set": {'loclist':loclist}})
-            songlist.find_one_and_update({'code':code}, {"$set": {'downloads':len(dllist)}})
-        for i in songlist.find({'username':session['username']}):
-            mysonglist.append(i)
-        if(len(mysonglist) > 0):
-            return render_template("stat.html", list = mysonglist)
-        else:
-            return render_template("stat.html")
+	if 'username' not in session:
+		return render_template("home.html", message = "You must be logged in to your stats!")
+	else:
+		mysonglist = []
+		for i in songlist.find({'username':session['username']}):
+			song = i
+			code = song['code']
+			timelist = song['timelist']
+			loclist = song['loclist']
+			dllist = song['dllist']
+			for j in downloadlist.find({'scode':code}):
+				if j['scanned'] == True and j['dcode'] not in dllist:
+					#timelist.append(j['time'])
+					#loclist.append(j['loc'])
+					dllist.append(j['dcode'])
+			songlist.find_one_and_update({'code':code}, {"$set": {'timelist':timelist}})
+			songlist.find_one_and_update({'code':code}, {"$set": {'loclist':loclist}})
+			songlist.find_one_and_update({'code':code}, {"$set": {'downloads':len(dllist)}})
+		for i in songlist.find({'username':session['username']}):
+			mysonglist.append(i)
+		if(len(mysonglist) > 0):
+			return render_template("stat.html", list = mysonglist)
+		else:
+			return render_template("stat.html")
 
 @app.route('/stat/<code>')
 def statc(code):
-    if 'username' not in session:
-        return render_template("home.html", message = "You must be logged in to your stats!")
-    else:
-        song = {}
-        for i in songlist.find({'code':code}):
-            song = i
-        if song['username'] == session['username']:
-            timelist = song['timelist']
-            loclist = song['loclist']
-            dllist = song['dllist']
-            for j in downloadlist.find({'scode':code}):
-                if j['scanned'] == True and j['dcode'] not in dllist:
-                    timelist.append(j['time'])
-                    loclist.append(j['loc'])
-                    dllist.append(j['dcode'])
-            songlist.find_one_and_update({'code':code}, {"$set": {'dllist':dllist}})
-            songlist.find_one_and_update({'code':code}, {"$set": {'timelist':timelist}})
-            songlist.find_one_and_update({'code':code}, {"$set": {'loclist':loclist}})
-            songlist.find_one_and_update({'code':code}, {"$set": {'downloads':len(dllist)}})
-            for i in songlist.find({'code':code}):
-                song = i
-            return render_template("stat.html", song = song)
-        else:
-            return render_template("home.html", message = "Illegal Access!", username = session["username"])
+	if 'username' not in session:
+		return render_template("home.html", message = "You must be logged in to your stats!")
+	else:
+		song = {}
+		for i in songlist.find({'code':code}):
+			song = i
+		if song['username'] == session['username']:
+			timelist = song['timelist']
+			loclist = song['loclist']
+			dllist = song['dllist']
+			for j in downloadlist.find({'scode':code}):
+				if j['scanned'] == True and j['dcode'] not in dllist:
+					timelist.append(j['time'])
+					loclist.append(j['loc'])
+					dllist.append(j['dcode'])
+			songlist.find_one_and_update({'code':code}, {"$set": {'dllist':dllist}})
+			songlist.find_one_and_update({'code':code}, {"$set": {'timelist':timelist}})
+			songlist.find_one_and_update({'code':code}, {"$set": {'loclist':loclist}})
+			songlist.find_one_and_update({'code':code}, {"$set": {'downloads':len(dllist)}})
+			for i in songlist.find({'code':code}):
+				song = i
+			return render_template("stat.html", song = song)
+		else:
+			return render_template("home.html", message = "Illegal Access!", username = session["username"])
 
 
 
 
-    
+	
 
 
-    
+	
 
 
 @app.route('/logout')
 def logout_redirect():
-    session.pop('username', None)
-    return redirect("/")
+	session.pop('username', None)
+	return redirect("/")
 
 
 if __name__ == '__main__':
-    app.run(host=ip, port=port, debug=True)
+	app.run(host=ip, port=port, debug=True)
